b6949903f67271103a35fc2aa231094a8fd99609
mrszarak <90174522+mrszarak@users.noreply.github.com>
2023-11-26 20:20:29 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+        <title>Hotel "Pod Dzikiem"</title>
+<meta charset="utf8" lang="pl">
+<link rel="stylesheet" href="styl.css">
+<link rel="icon" type="image/x-icon" href="favicon.ico">
+    </head>
+    <body>
+        <br>
+        <header>
+            
+<img src="dzik.png">
+<h1 id="nazwa">Hotel "Pod Dzikiem"</h1>
+
+</header>
+<div id="menu">
+            <form action="ksiega_gosci.php" target="strony" id="ksiega">
+                <button type ="submit">Księga gości
+            </form>
+  
+
+
+            <form action="formularz_pytajacy.php" target="strony" id="wyszukiwarka">
+                <button type="submit">Wyszukiwarka gości
+            </form>
+ 
+
+    
+            <form action="formularz_wpisujacy.php" target="strony" id="wprowadzanie">
+                <button type="submit">Wprowadzanie gości
+            </form>
+
+
+
+
+        </div>  
+<div id="zawartosc">
+<iframe style="height:80%; width: 100%;" name="strony" src="ksiega_gosci.php" scrolling="no"></iframe>
+</div>
+    </body>
+</html>
---
+++ ksiega_gosci.php
@@ -0,0 +1,23 @@
+  <html>
+    <head>
+       
+<meta charset="utf8" lang="pl">
+<link rel="stylesheet" href="styl.css">
+    </head>
+    <body >
+      <div style="background-color:#0d1b2a; width: 100% height:100%;">
+
+        <?php
+     $con= mysqli_connect('fdb1030.awardspace.net','3720513_hotel','zaq1@WSX','3720513_hotel');
+     $kw= "Select id,imie,nazwisko,data_przyjazdu,data_wyjazdu,nr_pokoju from goscie;";
+     $drukowanie= mysqli_query($con, $kw);
+     while($row = mysqli_fetch_row($drukowanie))
+     {
+        echo "<h2>{$row[0]} . {$row[1]} {$row[2]} data zameldowania {$row[3]} data wymeldowania {$row[4]} nr pokoju:{$row[5]} </h2><br>  ";
+     }
+ 
+        ?>
+      </div>
+       
+    </body>
+</html>
